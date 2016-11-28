a1387313beafcc69376da0905a3ebaf2fb78b088
CAMPUS\af11oq <af11oq@brocku.ca>
2016-11-28 17:01:47 -0500

    added assignment 3

---
+++ assignment3.js
@@ -0,0 +1,290 @@
+function add(){
+  var word1 = document.getElementById("word1").value;
+  var word2 = document.getElementById("word2").value;
+
+  if (word1=="Zero"){
+    var num1 = 0;
+  }
+  else if (word1=="One"){
+    var num1 = 1;
+  }
+  else if (word1=="Two"){
+    var num1 = 2;
+  }
+  else if (word1=="Three"){
+    var num1 = 3;
+  }
+  else if (word1=="Four"){
+    var num1 = 4;
+  }
+  else if (word1=="Five"){
+    var num1 = 5;
+  }
+  else if (word1=="Six"){
+    var num1 = 6;
+  }
+  else if (word1=="Seven"){
+    var num1 = 7;
+  }
+  else if (word1=="Eight"){
+    var num1 = 8;
+  }
+  else if (word1=="Nine"){
+    var num1 = 9;
+  }
+
+
+
+
+  if (word2=="Zero"){
+    var num2 = 0;
+  }
+  else if (word2=="One"){
+    var num2 = 1;
+  }
+  else if (word2=="Two"){
+    var num2 = 2;
+  }
+  else if (word2=="Three"){
+    var num2 = 3;
+  }
+  else if (word2=="Four"){
+    var num2 = 4;
+  }
+  else if (word2=="Five"){
+    var num2 = 5;
+  }
+  else if (word2=="Six"){
+    var num2 = 6;
+  }
+  else if (word2=="Seven"){
+    var num2 = 7;
+  }
+  else if (word2=="Eight"){
+    var num2 = 8;
+  }
+  else if (word2=="Nine"){
+    var num2 = 9;
+  }
+
+  document.getElementById("output").innerHTML = (("Output: " + word1 + " plus " + word2 + " equals ") + (num1 + num2));
+
+}
+
+function subtract() {
+  var word1=document.getElementById("word1").value;
+  var word2=document.getElementById("word2").value;
+
+  if (word1=="Zero"){
+    var num1 = 0;
+  }
+  else if (word1=="One"){
+    var num1 = 1;
+  }
+  else if (word1=="Two"){
+    var num1 = 2;
+  }
+  else if (word1=="Three"){
+    var num1 = 3;
+  }
+  else if (word1=="Four"){
+    var num1 = 4;
+  }
+  else if (word1=="Five"){
+    var num1 = 5;
+  }
+  else if (word1=="Six"){
+    var num1 = 6;
+  }
+  else if (word1=="Seven"){
+    var num1 = 7;
+  }
+  else if (word1=="Eight"){
+    var num1 = 8;
+  }
+  else if (word1=="Nine"){
+    var num1 = 9;
+  }
+
+
+
+
+  if (word2=="Zero"){
+    var num2 = 0;
+  }
+  else if (word2=="One"){
+    var num2 = 1;
+  }
+  else if (word2=="Two"){
+    var num2 = 2;
+  }
+  else if (word2=="Three"){
+    var num2 = 3;
+  }
+  else if (word2=="Four"){
+    var num2 = 4;
+  }
+  else if (word2=="Five"){
+    var num2 = 5;
+  }
+  else if (word2=="Six"){
+    var num2 = 6;
+  }
+  else if (word2=="Seven"){
+    var num2 = 7;
+  }
+  else if (word2=="Eight"){
+    var num2 = 8;
+  }
+  else if (word2=="Nine"){
+    var num2 = 9;
+  }
+
+document.getElementById("output").innerHTML = (("Output: " + word1 + " Minus " + word2 + " equals ") + (num1 - num2));
+
+}
+
+function multiply() {
+  var word1=document.getElementById("word1").value;
+  var word2=document.getElementById("word2").value;
+
+  if (word1=="Zero"){
+    var num1 = 0;
+  }
+  else if (word1=="One"){
+    var num1 = 1;
+  }
+  else if (word1=="Two"){
+    var num1 = 2;
+  }
+  else if (word1=="Three"){
+    var num1 = 3;
+  }
+  else if (word1=="Four"){
+    var num1 = 4;
+  }
+  else if (word1=="Five"){
+    var num1 = 5;
+  }
+  else if (word1=="Six"){
+    var num1 = 6;
+  }
+  else if (word1=="Seven"){
+    var num1 = 7;
+  }
+  else if (word1=="Eight"){
+    var num1 = 8;
+  }
+  else if (word1=="Nine"){
+    var num1 = 9;
+  }
+
+
+
+
+  if (word2=="Zero"){
+    var num2 = 0;
+  }
+  else if (word2=="One"){
+    var num2 = 1;
+  }
+  else if (word2=="Two"){
+    var num2 = 2;
+  }
+  else if (word2=="Three"){
+    var num2 = 3;
+  }
+  else if (word2=="Four"){
+    var num2 = 4;
+  }
+  else if (word2=="Five"){
+    var num2 = 5;
+  }
+  else if (word2=="Six"){
+    var num2 = 6;
+  }
+  else if (word2=="Seven"){
+    var num2 = 7;
+  }
+  else if (word2=="Eight"){
+    var num2 = 8;
+  }
+  else if (word2=="Nine"){
+    var num2 = 9;
+  }
+
+document.getElementById("output").innerHTML = (("Output: " + word1 + " Times " + word2 + " equals ") + (num1 * num2));
+}
+
+function divide() {
+  var word1=document.getElementById("word1").value;
+  var word2=document.getElementById("word2").value;
+
+  if (word1=="Zero"){
+    var num1 = 0;
+  }
+  else if (word1=="One"){
+    var num1 = 1;
+  }
+  else if (word1=="Two"){
+    var num1 = 2;
+  }
+  else if (word1=="Three"){
+    var num1 = 3;
+  }
+  else if (word1=="Four"){
+    var num1 = 4;
+  }
+  else if (word1=="Five"){
+    var num1 = 5;
+  }
+  else if (word1=="Six"){
+    var num1 = 6;
+  }
+  else if (word1=="Seven"){
+    var num1 = 7;
+  }
+  else if (word1=="Eight"){
+    var num1 = 8;
+  }
+  else if (word1=="Nine"){
+    var num1 = 9;
+  }
+
+
+
+
+  if (word2=="Zero"){
+    var num2 = 0;
+  }
+  else if (word2=="One"){
+    var num2 = 1;
+  }
+  else if (word2=="Two"){
+    var num2 = 2;
+  }
+  else if (word2=="Three"){
+    var num2 = 3;
+  }
+  else if (word2=="Four"){
+    var num2 = 4;
+  }
+  else if (word2=="Five"){
+    var num2 = 5;
+  }
+  else if (word2=="Six"){
+    var num2 = 6;
+  }
+  else if (word2=="Seven"){
+    var num2 = 7;
+  }
+  else if (word2=="Eight"){
+    var num2 = 8;
+  }
+  else if (word2=="Nine"){
+    var num2 = 9;
+  }
+
+document.getElementById("output").innerHTML = (("Output: " + word1 + " Divide " + word2 + " equals ") + (num1 / num2));
+
+}
